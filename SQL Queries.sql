dbddb9ab7f1d83f711566dd061ae85b4d382d470
Ricky Santos <rickurin@gmail.com>
2024-12-29 10:54:04 -0500

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -18,3 +18,39 @@ Left Join refund_column
   on refund_column.order_id = order_status.order_id
 Where DATE_DIFF(refund_ts, ship_ts, day) > 30
   AND refund_column.refunded = 1
+
+-- What products have the top return rates?
+
+# products (distinct)
+# avg of return rate (case when)
+# join orders to oder_status
+
+Select DISTINCT(product_name),
+  ROUND(AVG(CASE WHEN refund_ts IS NOT NULL THEN 1 ELSE 0 END), 2) as refund_rate,
+  Round(Avg(USD_Price),2) as AOV
+FROM core.orders
+Join core.order_status
+  ON orders.id = order_status.order_id
+group by 1
+order by 2 desc
+
+-- what is the average delivery time across the years?
+
+# years colums (extract), group by 1
+# avg (datediff) dilevery_ts, ship_ts
+#left join order_status, orders.id = order_status.order_id
+
+Select extract(Year FROM orders.purchase_ts) as purchase_year,
+  avg(date_diff(delivery_ts, orders.purchase_ts, day)) as avg_delivery_time_days
+FROM core.orders
+Left join core.order_status
+  on orders.id = order_status.order_id
+group by 1
+order by 1 asc
+
+-- What is the average ship to refund time?
+
+# datediff refund_ts, delivery_ts
+
+Select AVG(DATE_DIFF(refund_ts, ship_ts, day)) as avg_refund_time
+from core.order_status
